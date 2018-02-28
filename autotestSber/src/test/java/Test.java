dfa559215a92017c39614f6b162994a8ab986d3d
sharik08 <shars08ru@gmail.com>
2018-02-28 04:51:19 +0300

    commit

--- autotestSber/src/test/java/Test.java
+++ autotestSber/src/test/java/Test.java
@@ -1,11 +1,12 @@
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
 
 public class Test {
     private WebDriver driver;
@@ -21,15 +22,30 @@ public class Test {
         driver.quit();
     }
     @org.junit.Test
-    public void test1() {
-        driver.findElement(By.xpath("//*[text()='Москва']")).click();
+    public void scrollFooter() {
+        driver.findElement(By.xpath(".//*[@class='region-list__name']")).click();
         Wait<WebDriver> wait = new WebDriverWait(driver,5,1000);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(".//*[@class='region-list__modal-caption']"))));
-        driver.findElement(By.xpath(".//a[contains(text(), 'Нижегородская область')]")).click();
-//        WebElement element = driver.findElement(By.id("region-list__select-widget_3224fb10-6bde-450d-a126-cca402633669"));
-//        Select select = new Select(element);
-//        select.selectByVisibleText("Нижегородская область");
-//        select.getFirstSelectedOption();
+        driver.findElement(By.xpath(".//*[@placeholder='Введите название региона']")).sendKeys("Нижегородская область");
+        driver.findElement(By.xpath(".//*[@class='region-search-box__option']")).click();
+        assertEquals("ошибка","Нижегородская область",driver.findElement(By.xpath(".//*[@class='region-list__name']")).getText());
+        WebElement webElem = driver.findElement(By.xpath(".//*[@class = 'sbrf-div-list-inner --area bp-area footer-white']"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(false);",webElem);
+        check("fb']",webElem,"facebook");
+        check("tw']",webElem,"twitter");
+        check("yt']",webElem,"youtube");
+        check("ins']",webElem,"instagram");
+        check("vk']",webElem,"vk");
+        check("ok']",webElem,"odnoklassniki");
+    }
+    private void check(String iconPathEnd, WebElement webElem,String message){
+        String iconPath = ".//*[@class='social__icon social__icon_type_";
+        try {
+            webElem.findElement(By.xpath(iconPath + iconPathEnd));
+            }
+            catch (NoSuchElementException e){
+                System.out.println("Нет значка "+message);
+            }
     }
 
 }
